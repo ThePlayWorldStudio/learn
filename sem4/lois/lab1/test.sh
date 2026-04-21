b2b23de52e7ab5240c64f7de85db311b15e623e1
baka <nope>
2026-04-21 22:26:42 +0300

    lois lab1

--- sem4/lois/lab1/test.sh
+++ sem4/lois/lab1/test.sh
@@ -1,3 +1,6 @@
-g++ -O0 --coverage tests/test_logictool.cpp src/logictool.cpp -o run_tests -lgtest -lgtest_main -pthread
-./run_tests
-lcov --remove coverage.info '*/gtest/*' '*/test_logictool.cpp' --output-file coverage.info
+rm -f *.gcda && \
+g++ -O0 --coverage tests/test_logictool.cpp src/logictool.cpp -o run_tests -lgtest -lgtest_main -pthread && \
+./run_tests && \
+lcov --capture --directory . --output-file coverage.info --quiet && \
+lcov --remove coverage.info '*/gtest/*' --output-file coverage.info && \
+lcov --summary coverage.info

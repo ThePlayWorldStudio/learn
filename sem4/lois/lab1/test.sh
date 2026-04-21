ee87d039d4816f54d5dbae37d86f5fdca5bb9955
bruh <nope>
2026-04-21 22:30:04 +0300

    lab1

--- sem4/lois/lab1/test.sh
+++ sem4/lois/lab1/test.sh
@@ -4,3 +4,4 @@ g++ -O0 --coverage tests/test_logictool.cpp src/logictool.cpp -o run_tests -lgte
 lcov --capture --directory . --output-file coverage.info --quiet && \
 lcov --remove coverage.info '*/gtest/*' --output-file coverage.info && \
 lcov --summary coverage.info
+rm -f *.gcda && \
